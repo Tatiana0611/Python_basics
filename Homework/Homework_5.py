1d0b9dfaae6a7097465c93e473be351b9fbe7740
Tatiana <tatiana-bondareva@inbox.ru>
2022-10-13 11:23:04 +0300

    change Homework_5 tesk 2

--- Homework/Homework_5.py
+++ Homework/Homework_5.py
@@ -18,28 +18,137 @@ print(res)
 # a) Добавьте игру против бота
 # b) Подумайте как наделить бота ""интеллектом""
 
-from random import randint
 
-total_sweets = 2021
+# 1 Вариант
 
-player_1 = input('Имя первого игрока: ')
-player_2 = input('Имя второго игрока: ')
-players = [player_1, player_2]
+# from random import randint
 
-count = randint(0, 1)
-print(f'Первый ход у игрока {players[count % 2]}')
+# total_sweets = 2021
 
-# Когда остается 28 конфет игрок, чей ход является следующим, забирает их все и выигрывает.
-while total_sweets > 28:
-    take_sweets = int(input(f'\n{players[count % 2]}, возьмите не более 28 конфет. Введите количество конфет: '))
-    total_sweets -= take_sweets
-    print(f'Количество оставшихся конфет: {total_sweets}')
-    count += 1
-else:
-    print(f'\n{players[count % 2]} берет оставшиеся конфеты в количестве {total_sweets} штук и побеждает!')
+# player_1 = input('Имя первого игрока: ')
+# player_2 = input('Имя второго игрока: ')
+# players = [player_1, player_2]
+
+# count = randint(0, 1)
+# print(f'Первый ход у игрока {players[count % 2]}')
+
+# # Когда остается 28 конфет игрок, чей ход является следующим, забирает их все и выигрывает.
+# while total_sweets > 28:
+#     take_sweets = int(input(f'\n{players[count % 2]}, возьмите не более 28 конфет. Введите количество конфет: '))
+#     total_sweets -= take_sweets
+#     print(f'Количество оставшихся конфет: {total_sweets}')
+#     count += 1
+# else:
+#     print(f'\n{players[count % 2]} берет оставшиеся конфеты в количестве {total_sweets} штук и побеждает!')
 
 # Чтобы забрать все конфеты первый игрок должен всегда брать конфеты в количестве 
 # (общее кол-во конфет % (максимальное кол-во конфет за 1 ход + 1)) штук
+
+
+# 2 Вариант
+
+from random import randint
+
+CNT_CANDY = 2021
+CNT_PLAYERS = 2
+MAX_CANDY_STEP = 28
+MIN_CANDY_STEP = 0
+
+PLAYERS_NAMES = {
+    1: 'Player_1',
+    2: 'Player_2'
+}
+
+def get_first_move_player() -> int:
+    """
+    Случайный выбор игрока, который будет ходить первым
+    :return: номер игрока (1/2)
+    """
+    return randint(1, CNT_PLAYERS)
+
+def valid_cnt_candy(cnt: int) -> bool:
+    """
+    Проверка количества выбранных пользователем конфет
+    :param cnt: количество конфет
+    :return: True/False
+    """
+
+    if MIN_CANDY_STEP <= cnt <= MAX_CANDY_STEP:
+        return True
+    else:
+        return False
+
+def valid_type_value_cnt_candy(cnt: str) -> bool:
+    """
+    Введенное пользователем значение - целое число?
+
+    :param cnt: введенное пользователем значение
+    :return: True / False
+    """
+
+    if cnt.isdigit():
+        return True
+    else:
+        return False
+
+def valid_value_cnt_candy(cnt: int, full_cnt: int, players_candies: dict) -> bool:
+    """
+    Не превышает введенное пользователем значение количество
+    оставшихся конфет
+    :param players_candies: количество конфет на руках у игроков
+    :param full_cnt: всего конфет
+    :param cnt: желаемое пользователем количество конфет
+    :return: True/False
+    """
+
+    if (full_cnt - (players_candies[1] + players_candies[2])) < cnt:
+        return False
+    else:
+        return True
+
+def get_remainder(players_candies: dict, full_cnt: int) -> int:
+    """
+    Подсчет количества оставшихся конфет
+    :param players_candies: количество конфет на руках у игроков
+    :param full_cnt: всего конфет
+    :return: количество оставшихся конфет
+    """
+
+    return full_cnt - (players_candies[1] + players_candies[2])
+
+def start_game_human_human():
+    """
+    Игра в режиме человек с человеком
+    :return: None
+    """
+    cnt_candy_players: dict = {
+        1: 0,
+        2: 0
+    }
+
+    id_player: int = get_first_move_player()
+
+    while (cnt_candy_players[1] + cnt_candy_players[2]) < CNT_CANDY:
+        current_cnt_candy = input(f"{PLAYERS_NAMES[id_player]}, Ваш ход: ")
+
+        if not valid_type_value_cnt_candy(current_cnt_candy) or not valid_cnt_candy(int(current_cnt_candy)):
+            print(f"Допустимое число конфет [{MIN_CANDY_STEP} , {MAX_CANDY_STEP}]")
+            continue
+
+        if not valid_value_cnt_candy(int(current_cnt_candy), CNT_CANDY, cnt_candy_players):
+            print(f"{PLAYERS_NAMES[id_player]}, Вы не можете взять больше конфет, чем осталось")
+            continue
+
+        cnt_candy_players[id_player] += int(current_cnt_candy)
+
+        print(f"Осталось конфет: {get_remainder(cnt_candy_players, CNT_CANDY)}")
+
+        id_player = 1 if id_player == 2 else 2
+
+    print(f"Победу одержал {PLAYERS_NAMES[1 if id_player == 2 else 2]}")
+
+
+
 
 
 
